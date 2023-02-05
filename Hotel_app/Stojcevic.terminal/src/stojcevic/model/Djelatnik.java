e0cbeeb679a70033d48a0b838fddc25288c11884
Toni Stojčević <stojcevict@gmail.com>
2023-02-05 19:26:05 +0100

    Izmjene u klasi

--- Hotel_app/Stojcevic.terminal/src/stojcevic/model/Djelatnik.java
+++ Hotel_app/Stojcevic.terminal/src/stojcevic/model/Djelatnik.java
@@ -1,22 +1,17 @@
 package stojcevic.model;
 
 public class Djelatnik extends Entitet {
-	
+
 	private String ime;
 	private String prezime;
 	private String brojUgovora;
 	private String OIB;
 	private radnoMjesto radnoMjesto;
-	
-	
-	
+
 	public Djelatnik() {
 		super();
 	}
-	
-	
-	
-	
+
 	public Djelatnik(int sifra, String ime, String prezime, String brojUgovora, String oIB,
 			stojcevic.model.radnoMjesto radnoMjesto) {
 		super(sifra);
@@ -27,42 +22,50 @@ public class Djelatnik extends Entitet {
 		this.radnoMjesto = radnoMjesto;
 	}
 
-
-
-
-	
-
-
-
 	public String getIme() {
 		return ime;
 	}
+
 	public void setIme(String ime) {
 		this.ime = ime;
 	}
+
 	public String getPrezime() {
 		return prezime;
 	}
+
 	public void setPrezime(String prezime) {
 		this.prezime = prezime;
 	}
+
 	public String getBrojUgovora() {
 		return brojUgovora;
 	}
+
 	public void setBrojUgovora(String brojUgovora) {
 		this.brojUgovora = brojUgovora;
 	}
+
 	public String getOIB() {
 		return OIB;
 	}
+
 	public void setOIB(String oIB) {
 		OIB = oIB;
 	}
+
 	public radnoMjesto getRadnoMjesto() {
 		return radnoMjesto;
 	}
+
 	public void setRadnoMjesto(radnoMjesto radnoMjesto) {
 		this.radnoMjesto = radnoMjesto;
 	}
 
+	@Override
+	public String toString() {
+		return "(ime: " + ime + "	prezime: " + prezime + "	OIB: " + OIB + "	Broj_Ugovora: " + brojUgovora + ")";
+		
+	}
+	
 }
