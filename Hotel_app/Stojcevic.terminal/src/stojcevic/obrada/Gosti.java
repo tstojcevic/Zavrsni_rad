6866d59335cbfa3d84ecda0e00d13ed98fc598ef
Toni Stojčević <stojcevict@gmail.com>
2023-02-06 20:52:37 +0100

    brisanje viška redova

--- Hotel_app/Stojcevic.terminal/src/stojcevic/obrada/Gosti.java
+++ Hotel_app/Stojcevic.terminal/src/stojcevic/obrada/Gosti.java
@@ -8,38 +8,34 @@ import stojcevic.Start;
 import stojcevic.model.Gost;
 
 public class Gosti {
-	
+
 	private List<Gost> gosti;
 	private Start start;
-	
-	
-	
+
 	public Gosti(Start start) {
 		super();
 		this.start = start;
 		gosti = new ArrayList<>();
 		testPodaci();
-		}
-	
-	
+	}
+
 	public Gosti(Start start, List<Gost> gosti) {
 		super();
 		this.start = start;
 		this.gosti = gosti;
 		testPodaci();
 	}
-	
-private void testPodaci() {
-		if(Pomocno.DEV) {
+
+	private void testPodaci() {
+		if (Pomocno.DEV) {
 			gosti.add(new Gost(1, "Ivica", "Ivić", "44036942226", "iivic@gmail.com", "+385981234567"));
 			gosti.add(new Gost(2, "Marija", "Marijanović", "06442453523", "mmarijanovic@gmail.com", "+385951486277"));
-			gosti.add(new Gost(3, "Ksenija", "Horvat", "06442453523", "khorvat@gmail.com", "+385951486277"));			
+			gosti.add(new Gost(3, "Ksenija", "Horvat", "06442453523", "khorvat@gmail.com", "+385951486277"));
 		}
 	}
 
+	public void izbornik() {
 
-public void izbornik() {
-		
 		System.out.println("");
 		System.out.println("Gost izbornik");
 		System.out.println("Dostupne opcije");
@@ -49,14 +45,11 @@ public void izbornik() {
 		System.out.println("4. Brisanje gosta");
 		System.out.println("5. Povratak na glavni izbornik");
 		odabirIzbornika();
-		
+
 	}
-	
-	
-	
-	
+
 	private void odabirIzbornika() {
-		switch(Pomocno.unosBrojRaspon("Odaberite opciju: ", 1, 5)) {
+		switch (Pomocno.unosBrojRaspon("Odaberite opciju: ", 1, 5)) {
 		case 1:
 			pregled(true);
 			break;
@@ -64,56 +57,51 @@ public void izbornik() {
 			unosNovogGosta();
 			break;
 		case 3:
-			if(gosti.size()==0) {
+			if (gosti.size() == 0) {
 				System.out.println("Nema gosta kojeg bi mijenali");
 				izbornik();
-			}else {
+			} else {
 				promjena();
 			}
 			break;
 		case 4:
-			if(gosti.size()==0) {
+			if (gosti.size() == 0) {
 				System.out.println("Nema gosta kojeg bi obrisali");
 				izbornik();
-			}else {
+			} else {
 				brisanje();
 			}
 			break;
 		case 5:
 			start.glavniIzbornik();
 		}
-	
-	
-}
 
+	}
 
 	private void brisanje() {
 		pregled(false);
 		int rb = Pomocno.unosBrojRaspon("Odaberite gosta kojeg želite obrisati: ", 1, gosti.size());
-		gosti.remove(rb-1);
+		gosti.remove(rb - 1);
 		izbornik();
 	}
-
 
 	private void promjena() {
 		pregled(false);
 		int rb = Pomocno.unosBrojRaspon("Unesite gosta kojeg želite promijeniti: ", 1, gosti.size());
-		Gost g = gosti.get(rb-1);
+		Gost g = gosti.get(rb - 1);
 		g.setIme(Pomocno.unosTeksta("Unesite ime gosta: "));
 		g.setPrezime(Pomocno.unosTeksta("Unesite prezime gosta"));
 		g.setOIB(Pomocno.unosTeksta("Unesite OIB gosta: "));
 		g.setEmail(Pomocno.unosTeksta("Unesite e-mail gosta: "));
 		g.setKontakTelefon(Pomocno.unosTeksta("Unesite kontakt telefon gosta: "));
-		
+
 		izbornik();
 	}
-
 
 	private void unosNovogGosta() {
 		gosti.add(unesiNovogGosta());
 		izbornik();
 	}
-
 
 	private Gost unesiNovogGosta() {
 		Gost g = new Gost();
@@ -126,11 +114,10 @@ public void izbornik() {
 		return g;
 	}
 
-
 	private void pregled(boolean prikaziIzbornik) {
 		System.out.println("Gosti u aplikaciji:");
 		int rb = 1;
-		for (Gost g : gosti ) {
+		for (Gost g : gosti) {
 			System.out.println(rb++ + ". " + g);
 		}
 		if (prikaziIzbornik) {
@@ -138,14 +125,12 @@ public void izbornik() {
 		}
 	}
 
-
 	public List<Gost> getGosti() {
 		return gosti;
 	}
+
 	public void setGosti(List<Gost> gosti) {
 		this.gosti = gosti;
 	}
-	
-	
 
 }
